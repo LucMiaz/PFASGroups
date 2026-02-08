b78920ef609844412024c117ef5de3564fcd9661
Luc T. Miaz <luc.miaz@aces.su.se>
2026-02-08 15:11:00 +0100

    changed imports

--- PFASgroups/__init__.py
+++ PFASgroups/__init__.py
@@ -2,8 +2,8 @@
 from .PFASGroupModel import PFASGroup
 from .PFASDefinitionModel import PFASDefinition
 from .ComponentsSolverModel import ComponentsSolver
-from .core import rdkit_disable_log
-from .parser import parse_smiles, parse_mols, parse_mol,parse_groups_in_mol,  compile_componentSmarts, compile_componentSmartss
+from .core import rdkit_disable_log, PFAS_GROUPS_FILE
+from .parser import parse_smiles, parse_mols, parse_mol,parse_groups_in_mol,  compile_componentSmarts, compile_componentSmartss,load_PFASGroups
 from .draw_mols import plot_mol, plot_mols, plot_pfasgroups
 from .getter import get_componentSmartss, get_PFASGroups, get_PFASDefinitions
 from .fingerprints import generate_fingerprint
@@ -19,5 +19,5 @@ except ImportError:
     pass
 
 __version__ = "2.2.1"
-__all__ = ['PFASGroup', 'PFASDefinition', 'parse_smiles', 'parse_mols','parse_mol', 'parse_groups_in_mol', 'plot_pfasgroups', 'plot_mol','plot_mols',"generate_fingerprint", 'get_componentSmartss', 'get_PFASGroups', 'get_PFASDefinitions' ,'compile_componentSmarts', 'compile_componentSmartss','ComponentsSolver', 'generate_homologues', 'generate_degradation_products',"rdkit_disable_log"]
+__all__ = ['PFASGroup', 'PFASDefinition', 'parse_smiles', 'parse_mols','parse_mol', 'parse_groups_in_mol', 'plot_pfasgroups', 'plot_mol','plot_mols',"generate_fingerprint", 'get_componentSmartss', 'get_PFASGroups', 'get_PFASDefinitions' ,'compile_componentSmarts', 'compile_componentSmartss','ComponentsSolver', 'generate_homologues', 'generate_degradation_products',"rdkit_disable_log","load_PFASGroups", "PFAS_GROUPS_FILE"]
 __all__.append('ResultsModel')
